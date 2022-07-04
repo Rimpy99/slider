356a72870a0d6d2b2d5e1f6d6626505e78b3f589
Rimpy99 <70006pawel@gmail.com>
2022-07-04 09:52:58 +0200

    Slides and transitions created

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
+import { useState, useEffect } from 'react';
 import './App.css';
-import ArrowLeft from './components/ArrowLeft';
-import ArrowRight from './components/ArrowRight';
-// import ArrowLeft from './components/ArrowLeft';
 
 const App: React.FC = () => {
+  const [x, setX] = useState<number>(0);
+
+  const goLeft = () => {
+    setX(x + 100)
+  }
+
+  const goRight = () => {
+    setX(x - 100)
+  }
+
+  // useEffect(()=>{
+  //   if(x===-300){
+  //     setTimeout(()=>{
+  //       setX(0);
+  //     },5000)
+  //   }
+  // },[x])
+
   return (
     <div className="App">
       <div className="slider">
-        <p>Lorem, ipsum</p>
-        <p>Lorem ipsum dolor sit</p>
-        <ArrowLeft/>
-        <ArrowRight/>
+        <div className="slide slide1" style={{transform:`translateX(${x}%)`}}>
+          <div  className="slide-content">
+            <h1>RED</h1>
+          </div>
+        </div>
+        <div className="slide slide2" style={{transform:`translateX(${x}%)`}}>
+          <div  className="slide-content">
+            <h1>GREEN</h1>
+          </div>
+        </div>
+        <div className="slide slide3" style={{transform:`translateX(${x}%)`}}>
+          <div  className="slide-content">
+            <h1>YELLOW</h1>
+          </div>
+        </div>
+        <div className="slide slide4" style={{transform:`translateX(${x}%)`}}>
+          <div  className="slide-content">
+            <h1>BLUE</h1>
+          </div>
+        </div>
       </div>
-      <div className="slider">
-        <p>Lorem ipsum dolor sit</p>
-        <p>Lorem, ipsum</p>
-        <ArrowLeft/>
-        <ArrowRight/>
-      </div>
-      <div className="slider">
-        <p>Lorem ipsum dolor sit, amet</p>
-        <p>Lorem</p>
-        <ArrowLeft/>
-        <ArrowRight/>
-      </div>
-      <div className="slider">
-        <p>Lorem, ipsum</p>
-        <p>Lorem, ipsum dolor</p>
-        <ArrowLeft/>
-        <ArrowRight/>
-      </div>
+      {x===0 ? null :  <button className="btn-left" onClick={goLeft}>Left</button>}
+      {x===-300 ? null : <button className="btn-right" onClick={goRight}>Right</button>}
     </div>
   );
 }
